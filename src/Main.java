9076d619985b6106f1c280141b1f3301daefaf5f
Gibson <gibschrt16@gmail.com>
2025-03-24 12:31:44 +0300

    firt commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        MyVariables var  = new MyVariables();
+        var.calculate();
+        var.join_names();
+
+        MyDatStrct strct = new MyDatStrct();
+
+        String[]names = new String[0];
+
+        strct.setNames();
+        strct.setPerson();
+        strct.setAges();
+
+
+
+    }
+}
+
